c57dcaf5d446fd454d1be4dc21bde202bd8ab5aa
Trichardson20 <148395400+Trichardson20@users.noreply.github.com>
2024-02-01 14:01:14 -0800

    Manual controls interface is working, needs some adjustments

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import { ButtonGroup, Menu } from '@mui/material';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Button, Box, ButtonGroup, Select, InputLabel, MenuItem, Radio, RadioGroup, FormControlLabel, Grid, Paper, Typography, FormControl, FormLabel } from '@mui/material';
 import Stack from '@mui/material/Stack';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import ROSLIB from "roslib";
 import { RosConnection, rosImageSrcString } from 'rosreact';
 import { useROS } from './ros-helpers';
@@ -16,11 +13,18 @@ function App() {
   let { ros } = useROS();
   const [part, setPart] = React.useState('');
   const [currentStatus, setCurrentStatus] = React.useState("Not Connected")
-    
+  const [tabValue, setTabValue] = React.useState("Automated")
+  const [controlMode, setControlMode] = React.useState('Movement');
+
+  const handleControlChange = (event) => {
+    setControlMode(event.target.value);
+  };
   const handleChange = (event) => {
     setPart(event.target.value);
   };
-
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
   // console.log(ros)
 
   // React.useEffect(() => {
@@ -35,7 +39,7 @@ function App() {
   
 
   const moveUp = () => {
-    if (part == "Base") {
+    if (part === "Base") {
       // var ros = new ROSLIB.Ros({
       //   url: 'ws://slinky.hcrlab.cs.washington.edu:9090'
       // })
@@ -60,7 +64,7 @@ function App() {
   }
 
   const moveLeft = () => {
-    if (part == "Base") {
+    if (part === "Base") {
       var ros = new ROSLIB.Ros({
         url: 'ws://slinky.hcrlab.cs.washington.edu:9090'
       })
@@ -85,7 +89,7 @@ function App() {
   }
 
   const moveRight = () => {
-    if (part == "Base") {
+    if (part === "Base") {
       var ros = new ROSLIB.Ros({
         url: 'ws://slinky.hcrlab.cs.washington.edu:9090'
       })
@@ -110,7 +114,7 @@ function App() {
   }
 
   const moveDown = () => {
-    if (part == "Base") {
+    if (part === "Base") {
       var ros = new ROSLIB.Ros({
         url: 'ws://slinky.hcrlab.cs.washington.edu:9090'
       })
@@ -136,29 +140,73 @@ function App() {
 
   return (
     <center>
-    {/* <RosConnection url="wss://slinky.hcrlab.cs.washington.edu:9090" autoConnect> */}
     <RosConnection url="ws://slinky.hcrlab.cs.washington.edu:9090" autoConnect>
       <h1>CSE 481C Stretch Web Interface</h1>
-      <h4>Current Status: Connected</h4>
-      <header className="App-header">
-      <Stack spacing={4}>
-        <FormControl variant="standard" fullWidth>
-          <InputLabel id="demo-simple-select-label">Select Part</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={part}
-            label="Part"
-            onChange={handleChange}
-          >
-            <MenuItem value="Arm">Arm</MenuItem>
-            <MenuItem value="Base">Base</MenuItem>
-            <MenuItem value="Wrist">Wrist</MenuItem>
-            <MenuItem value="Camera">Camera</MenuItem>
-          </Select>
-        </FormControl>
-      
-        <Stack>
+      <h4>Current Status: {currentStatus}</h4>
+      <AppBar position="static" sx={{ backgroundColor: '#ffffff', boxShadow: 3 }}>
+        <Tabs value={tabValue} onChange={handleTabChange} aria-label="simple tabs example">
+          <Tab label="Automated" value="Automated" />
+          <Tab label="Manual" value="Manual" />
+        </Tabs>
+      </AppBar>
+
+      {tabValue === "Automated" && (
+        <Stack spacing={4}>
+          <Button variant="contained" color="primary">Clean</Button>
+          <Button variant="contained" color="secondary">Return Home</Button>
+        </Stack>
+      )}
+
+      {tabValue === "Manual" && (
+        <Paper style={{ padding: 20 }}>
+          <Typography variant="h6">Manual Control</Typography>
+          <div 
+  style={{
+    position: 'absolute', left: '50%', top: '28%',
+    transform: 'translate(-50%, -50%)',
+    marginTop: 5,
+    margin: 6
+    
+}}>
+          <FormControl variant="standard" fullWidth sx={{ display: 'flex', justifyContent: 'center', marginBottom: 2 }}>
+            <RadioGroup row name="controlMode" value={controlMode} onChange={handleControlChange} >
+              <FormControlLabel value="Movement" control={<Radio />} label="Movement" />
+              <FormControlLabel value="Arm" control={<Radio />} label="Arm" />
+            </RadioGroup>
+          </FormControl>
+          </div>
+          
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', padding: 8 }}>
+      {/* Directional pad for Movement */}
+      {controlMode === "Movement" && (
+       <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
+       <Stack spacing={2} alignItems="center">
+       <Typography variant="subtitle1">Base</Typography>
+         <Button variant="contained" onClick={moveUp}>Forward</Button>
+         <ButtonGroup variant="contained" aria-label="outlined button group">
+           <Button onClick={moveLeft}>Left</Button>
+           <Button onClick={moveRight}>Right</Button>
+         </ButtonGroup>
+         <Button variant="contained" onClick={moveDown}>Backward</Button>
+       </Stack>
+        
+       <Stack spacing={2} alignItems="center">
+       <Typography variant="subtitle1"> Head Camera</Typography>
+         <Button variant="contained" onClick={null}>Tilt Up</Button>
+         <ButtonGroup variant="contained" aria-label="outlined button group">
+           <Button onClick={null}>Left</Button>
+           <Button onClick={null}>Right</Button>
+         </ButtonGroup>
+         <Button variant="contained" onClick={null}>Tilt Down</Button>
+       </Stack>
+     </Box>
+      )}
+
+      {/* Directional pad for Arm */}
+      {controlMode === "Arm" && (
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
+        <Stack spacing={2} alignItems="center">
+        <Typography variant="subtitle1">Arm</Typography>
           <Button variant="contained" onClick={moveUp}>Up</Button>
           <ButtonGroup variant="contained" aria-label="outlined button group">
             <Button onClick={moveLeft}>Left</Button>
@@ -166,12 +214,23 @@ function App() {
           </ButtonGroup>
           <Button variant="contained" onClick={moveDown}>Down</Button>
         </Stack>
-        <Button variant="outlined">Stop</Button>
-      </Stack>
-      
-      </header>
+         
+        <Stack spacing={2} alignItems="center">
+        <Typography variant="subtitle1">Grabber</Typography>
+          <Button variant="contained" onClick={moveUp}>Out</Button>
+          <ButtonGroup variant="contained" aria-label="outlined button group">
+            <Button onClick={moveLeft}>Close</Button>
+            <Button onClick={moveRight}>Open</Button>
+          </ButtonGroup>
+          <Button variant="contained" onClick={moveDown}>In</Button>
+        </Stack>
+      </Box>
+      )}
+    </Box>
+  </Paper>
+)}
     </RosConnection>
-    </center>
+  </center>
   );
 }
 
