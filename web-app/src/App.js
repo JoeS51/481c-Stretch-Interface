c28d0a0c84ddfeb035c7168a69130b1cdc6c3a7b
Johannes Sluis <joesluis@gmail.com>
2024-02-21 16:47:18 -0800

    added a dropdown

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -136,7 +136,7 @@ function App() {
         angular: { x: 0.0, y: 0.0, z: 0.0 }
       });
       cmdVelTopic.publish(twist);
-      
+
       console.log("moved base")
     } else {
       console.log("not in the correct part")
@@ -235,171 +235,182 @@ function App() {
     // var ros = new ROSLIB.Ros({
     //   url: 'ws://slinky.hcrlab.cs.washington.edu:9090'
     // })
-    if (rosConnected){
-    console.log("lift joint")
-    var jointLiftClient = new ROSLIB.ActionHandle({
-      ros : rosConnected,
-      name : '/stretch_controller/follow_joint_trajectory',
-      actionType : 'control_msgs/action/FollowJointTrajectory',
-    });
+    if (rosConnected) {
+      console.log("lift joint")
+      var jointLiftClient = new ROSLIB.ActionHandle({
+        ros: rosConnected,
+        name: '/stretch_controller/follow_joint_trajectory',
+        actionType: 'control_msgs/action/FollowJointTrajectory',
+      });
 
-    let newPos = jointStates[JOINTS.indexOf(joint_name)] + pos;
-    console.log("curr pose: " + jointStates[JOINTS.indexOf(joint_name)])
-    console.log("NEW POSE: " + newPos)
+      let newPos = jointStates[JOINTS.indexOf(joint_name)] + pos;
+      console.log("curr pose: " + jointStates[JOINTS.indexOf(joint_name)])
+      console.log("NEW POSE: " + newPos)
 
-    var goal = new ROSLIB.ActionGoal({
-      trajectory: {
-        header: {
-          stamp: {
-            secs: 0,
-            nsecs: 0
-          }
-        },
-        joint_names: [joint_name],
-        points: [
-          {
-            positions: [newPos],
-            time_from_start: {
+      var goal = new ROSLIB.ActionGoal({
+        trajectory: {
+          header: {
+            stamp: {
               secs: 0,
-              nsecs: 1
+              nsecs: 0
             }
-          }
-        ]
-      }
-    
-    });
-    
-    console.log(goal);
+          },
+          joint_names: [joint_name],
+          points: [
+            {
+              positions: [newPos],
+              time_from_start: {
+                secs: 0,
+                nsecs: 1
+              }
+            }
+          ]
+        }
 
-    // goal.on('feedback', function(feedback) {
-    //   console.log('Feedback: ' + feedback.sequence);
-    // });
-  
-    // goal.on('result', function(result) {
-    //   console.log('Final Result: ' + result.sequence);
-    // });
-  
-    jointLiftClient.createClient(goal);
-  }
-  
+      });
+
+      console.log(goal);
+
+      // goal.on('feedback', function(feedback) {
+      //   console.log('Feedback: ' + feedback.sequence);
+      // });
+
+      // goal.on('result', function(result) {
+      //   console.log('Final Result: ' + result.sequence);
+      // });
+
+      jointLiftClient.createClient(goal);
+    }
+
   }
 
   const savePosition = () => {
-    console.log(currLocName);
+    // call the callback from ros
   }
 
   let image = new window.Image();
   image.src = cameraSubscribed;
-  
+
 
   return (
     <center>
       {/* <RosConnection url="ws://slinky.hcrlab.cs.washington.edu:9090" autoConnect> */}
-        <h1>CSE 481C Stretch Web Interface</h1>
-        <h3>Current Status: {currentStatus}</h3>
-        <h3> Marker ID: {markers ? markers : "Not Found"} </h3>
-        <Button onClick={testActionServer}>TestActionServer</Button>
+      <h1>CSE 481C Stretch Web Interface</h1>
+      <h3>Current Status: {currentStatus}</h3>
+      <h3> Marker ID: {markers ? markers : "Not Found"} </h3>
+      <Button onClick={testActionServer}>TestActionServer</Button>
 
-        <div>
-          <input type="text" value={currLocName} onChange={(e) => setCurrLocName(e.target.value)} placeholder='Enter a position name'/>
-          <Button variant="contained" onClick={savePosition} style={{ marginLeft: "10px"}}>Save Position</Button>
-        </div>
+      <AppBar position="static" sx={{ backgroundColor: '#00ff00', boxShadow: 8, alignItems: 'center', paddingBottom: '10px', paddingTop: '10px' }}>
+        <Tabs value={tabValue} onChange={handleTabChange} aria-label="simple tabs example">
+          <Tab label="Automated" value="Automated" />
+          <Tab label="Manual" value="Manual" />
+        </Tabs>
+      </AppBar>
 
-        <AppBar position="static" sx={{ backgroundColor: '#00ff00', boxShadow: 8, alignItems: 'center', paddingBottom: '10px', paddingTop: '10px' }}>
-          <Tabs value={tabValue} onChange={handleTabChange} aria-label="simple tabs example">
-            <Tab label="Automated" value="Automated" />
-            <Tab label="Manual" value="Manual" />
-          </Tabs>
-        </AppBar>
+      {tabValue === "Automated" && (
+        <ThemeProvider theme={buttonTheme}>
+          <div>
+            <label for="cars">Choose a car:</label>
+            {/* Change the values of the dropdown */}
+            <select name="cars" id="cars">
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </div>
+          <ButtonGroup >
+            <ButtonGroup orientation='vertical' >
+              <Button variant="contained" color="customColor" style={{ marginTop: '28px', marginBottom: '78px', width: '250px', height: '75px' }}>Clean the Room</Button>
 
-        {tabValue === "Automated" && (
-          <ThemeProvider theme={buttonTheme}>
-            <ButtonGroup >
-              <ButtonGroup orientation='vertical' >
-                <Button variant="contained" color="customColor" style={{ marginTop: '28px', marginBottom: '78px', width: '250px', height: '75px' }}>Clean the Room</Button>
-
-                <Button variant="contained" color="secondary" style={{ width: '150px', height: '50px' }} >Return Home</Button>
-              </ButtonGroup>
-              <Button variant='contained' color="StopColor" style={{ marginLeft: '84px', marginTop: '28px', width: '350px', height: '200px' }} >Emergency Stop</Button>
+              <Button variant="contained" color="secondary" style={{ width: '150px', height: '50px' }} >Return Home</Button>
             </ButtonGroup>
+            <Button variant='contained' color="StopColor" style={{ marginLeft: '84px', marginTop: '28px', width: '350px', height: '200px' }} >Emergency Stop</Button>
+          </ButtonGroup>
+          
+        </ThemeProvider>
 
-          </ThemeProvider>
-        )}
 
-        {tabValue === "Manual" && (
-          <Paper style={{ padding: 20 }}>
-            <Typography variant="h6">Manual Control</Typography>
-            <div
-              // style={{
-              //   position: 'absolute', left: '50%', top: '28%', transform: 'translate(-50%, -50%)', marginTop: 5, margin: 6
-              // }}
-              style = {{
-                marginLeft: "45%"
-              }}
-              >
-              <FormControl variant="standard" fullWidth sx={{ display: 'flex', justifyContent: 'center', marginBottom: 2 }}>
-                <RadioGroup row name="controlMode" value={controlMode} onChange={handleControlChange} sx={{ marginTop: '35px' }}>
-                  <FormControlLabel value="Movement" control={<Radio />} label="Movement" />
-                  <FormControlLabel value="Arm" control={<Radio />} label="Arm" />
-                </RadioGroup>
-              </FormControl>
-            </div>
+      )}
 
-            <Box sx={{ display: 'flex', justifyContent: 'space-around', padding: 8 }}>
-              {/* Directional pad for Movement */}
-              {controlMode === "Movement" && (
-                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
-                  <Stack spacing={2} alignItems="center">
-                    <Typography variant="subtitle1">Base</Typography>
-                    <Button variant="contained" onMouseDown={moveUp} style={{ width: '100px', height: '37px' }}>Forward</Button>
-                    <ButtonGroup variant="contained" >
-                      <Button onClick={moveLeft} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
-                      <Button onClick={moveRight} style={{ width: '100px', height: '37px' }}>Right</Button>
-                    </ButtonGroup>
-                    <Button variant="contained" onClick={moveDown} style={{ width: '100px', height: '37px' }}>Backward</Button>
-                  </Stack>
+      {tabValue === "Manual" && (
+        <Paper style={{ padding: 20 }}>
+          <Typography variant="h6">Manual Control</Typography>
+          <div>
+            <input type="text" value={currLocName} onChange={(e) => setCurrLocName(e.target.value)} placeholder='Enter a position name' />
+            <Button variant="contained" onClick={savePosition} style={{ marginLeft: "10px" }}>Save Position</Button>
+          </div>
+          <div
+            // style={{
+            //   position: 'absolute', left: '50%', top: '28%', transform: 'translate(-50%, -50%)', marginTop: 5, margin: 6
+            // }}
+            style={{
+              marginLeft: "45%"
+            }}
+          >
+            <FormControl variant="standard" fullWidth sx={{ display: 'flex', justifyContent: 'center', marginBottom: 2 }}>
+              <RadioGroup row name="controlMode" value={controlMode} onChange={handleControlChange} sx={{ marginTop: '35px' }}>
+                <FormControlLabel value="Movement" control={<Radio />} label="Movement" />
+                <FormControlLabel value="Arm" control={<Radio />} label="Arm" />
+              </RadioGroup>
+            </FormControl>
+          </div>
 
-                  <Stack spacing={2} alignItems="center">
-                    <Typography variant="subtitle1"> Head Camera</Typography>
-                    <Button variant="contained" onClick={()=>testActionServer("joint_head_tilt", 0.1)} style={{ width: '100px', height: '37px' }}>Tilt Up</Button>
-                    <ButtonGroup variant="contained" aria-label="outlined button group" >
-                      <Button onClick={()=>testActionServer("joint_head_pan", 0.1)} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
-                      <Button onClick={()=>testActionServer("joint_head_pan", -0.1)} style={{ width: '100px', height: '37px' }}>Right</Button>
-                    </ButtonGroup>
-                    <Button variant="contained" onClick={()=>testActionServer("joint_head_tilt", -0.1)} style={{ width: '115px', height: '37px' }}>Tilt Down</Button>
-                  </Stack>
-                </Box>
-              )}
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', padding: 8 }}>
+            {/* Directional pad for Movement */}
+            {controlMode === "Movement" && (
+              <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
+                <Stack spacing={2} alignItems="center">
+                  <Typography variant="subtitle1">Base</Typography>
+                  <Button variant="contained" onMouseDown={moveUp} style={{ width: '100px', height: '37px' }}>Forward</Button>
+                  <ButtonGroup variant="contained" >
+                    <Button onClick={moveLeft} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
+                    <Button onClick={moveRight} style={{ width: '100px', height: '37px' }}>Right</Button>
+                  </ButtonGroup>
+                  <Button variant="contained" onClick={moveDown} style={{ width: '100px', height: '37px' }}>Backward</Button>
+                </Stack>
 
-              {/* Directional pad for Arm */}
-              {controlMode === "Arm" && (
-                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
-                  <Stack spacing={2} alignItems="center">
-                    <Typography variant="subtitle1">Arm</Typography>
-                    <Button variant="contained" onClick={()=>testActionServer("joint_lift", 0.075)} style={{ width: '100px', height: '37px' }}>Up</Button>
-                    <ButtonGroup variant="contained" aria-label="outlined button group" >
-                      <Button onClick={moveLeft} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
-                      <Button onClick={moveRight} style={{ width: '100px', height: '37px' }}>Right</Button>
-                    </ButtonGroup>
-                    <Button variant="contained" onClick={()=>testActionServer("joint_lift", -0.075)} style={{ width: '100px', height: '37px' }}>Down</Button>
-                  </Stack>
+                <Stack spacing={2} alignItems="center">
+                  <Typography variant="subtitle1"> Head Camera</Typography>
+                  <Button variant="contained" onClick={() => testActionServer("joint_head_tilt", 0.1)} style={{ width: '100px', height: '37px' }}>Tilt Up</Button>
+                  <ButtonGroup variant="contained" aria-label="outlined button group" >
+                    <Button onClick={() => testActionServer("joint_head_pan", 0.1)} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
+                    <Button onClick={() => testActionServer("joint_head_pan", -0.1)} style={{ width: '100px', height: '37px' }}>Right</Button>
+                  </ButtonGroup>
+                  <Button variant="contained" onClick={() => testActionServer("joint_head_tilt", -0.1)} style={{ width: '115px', height: '37px' }}>Tilt Down</Button>
+                </Stack>
+              </Box>
+            )}
 
-                  <Stack spacing={2} alignItems="center">
-                    <Typography variant="subtitle1">Grabber</Typography> 
-                    <Button variant="contained" onClick={()=>testActionServer("wrist_extension", 0.075)} style={{ width: '100px', height: '37px' }}>Out</Button>
-                    <ButtonGroup variant="contained" aria-label="outlined button group">
-                      <Button onClick={()=>testActionServer("joint_gripper_finger_left", -0.075)} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Close</Button>
-                      <Button onClick={()=>testActionServer("joint_gripper_finger_left", 0.075)} style={{ width: '100px', height: '37px' }}>Open</Button>
-                    </ButtonGroup>
-                    <Button variant="contained" onClick={()=>testActionServer("wrist_extension", -0.075)} style={{ width: '100px', height: '37px' }}>In</Button>
-                  </Stack>
-                </Box>
-              )}
-            </Box>
-            <img src={cameraSubscribed} x={0} y={0} width={400} height={400}/>
-          </Paper>
-        )}
-        
+            {/* Directional pad for Arm */}
+            {controlMode === "Arm" && (
+              <Box sx={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
+                <Stack spacing={2} alignItems="center">
+                  <Typography variant="subtitle1">Arm</Typography>
+                  <Button variant="contained" onClick={() => testActionServer("joint_lift", 0.075)} style={{ width: '100px', height: '37px' }}>Up</Button>
+                  <ButtonGroup variant="contained" aria-label="outlined button group" >
+                    <Button onClick={moveLeft} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Left</Button>
+                    <Button onClick={moveRight} style={{ width: '100px', height: '37px' }}>Right</Button>
+                  </ButtonGroup>
+                  <Button variant="contained" onClick={() => testActionServer("joint_lift", -0.075)} style={{ width: '100px', height: '37px' }}>Down</Button>
+                </Stack>
+
+                <Stack spacing={2} alignItems="center">
+                  <Typography variant="subtitle1">Grabber</Typography>
+                  <Button variant="contained" onClick={() => testActionServer("wrist_extension", 0.075)} style={{ width: '100px', height: '37px' }}>Out</Button>
+                  <ButtonGroup variant="contained" aria-label="outlined button group">
+                    <Button onClick={() => testActionServer("joint_gripper_finger_left", -0.075)} style={{ marginRight: ' 32px', width: '100px', height: '37px' }}>Close</Button>
+                    <Button onClick={() => testActionServer("joint_gripper_finger_left", 0.075)} style={{ width: '100px', height: '37px' }}>Open</Button>
+                  </ButtonGroup>
+                  <Button variant="contained" onClick={() => testActionServer("wrist_extension", -0.075)} style={{ width: '100px', height: '37px' }}>In</Button>
+                </Stack>
+              </Box>
+            )}
+          </Box>
+          <img src={cameraSubscribed} x={0} y={0} width={400} height={400} />
+        </Paper>
+      )}
+
       {/* </RosConnection> */}
     </center>
   );
